3b91bc86152a183004a17bf7c571f0ab2f95de3c
김용민 <dydals3440@gmail.com>
2024-07-20 23:39:39 +0900

    feat: Login Page UI

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,12 +1,19 @@
-import {useState} from 'react'
+import {Routes, Route} from "react-router-dom";
 
 import './App.css'
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
 function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className='p-4 h-screen flex items-center jstify-center'>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/signup' element={<SignUp/>}/>
+            </Routes>
+        </div>
     )
 }
 
