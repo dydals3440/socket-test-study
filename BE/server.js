2625471e79a26c876adef6ef50abb7341dabcb1b
김용민 <dydals3440@gmail.com>
2024-07-20 20:25:50 +0900

    feat: auth route setup

--- BE/server.js
+++ BE/server.js
@@ -21,6 +21,11 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+// import routes
+import authRoutes from "./routes/auth.routes";
+
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
     console.log("Server started on port" + PORT);
 });
