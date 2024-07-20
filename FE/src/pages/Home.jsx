fb1a09be7936fd394068cc37cdf5cd313b5cafb6
김용민 <dydals3440@gmail.com>
2024-07-21 00:26:55 +0900

    feat: Message Container UI

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "../components/sidebar/Sidebar.jsx";
+import MessageContainer from "../components/messages/MessageContainer.jsx";
 
 const Home = () => {
     return (
         <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200'>
             <Sidebar/>
+            <MessageContainer/>
         </div>
     );
 };
