fa2501dcdca5229bc7e8c7db130d1767f8de7d1e
김용민 <dydals3440@gmail.com>
2024-07-21 02:00:33 +0900

    feat: Get Conversation functionality

--- FE/src/components/messages/MessageContainer.jsx
+++ FE/src/components/messages/MessageContainer.jsx
@@ -1,21 +1,51 @@
 import Messages from "./Messages.jsx";
 import MessageInput from "./MessageInput.jsx";
+import useConversation from "../../zustand/useConversation.js";
+import {useEffect} from "react";
+import {TiMessages} from 'react-icons/ti'
 
 const MessageContainer = () => {
+    const {selectedConversation, setSelectedConversation} = useConversation();
+    console.log(selectedConversation);
+
+    useEffect(() => {
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation]);
+
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            <>
+            {!selectedConversation ? (
+                <NoChatSelected/>
+            ) : (<>
                 {/* Header */}
                 <div className='bg-slate-500 px-4 py-2 mb-2'>
                     <span className='label-text'>To: </span>
-                    <span className='text-gray-900 font-bold'>매튜</span>
+                    <span className='text-gray-900 font-bold'>
+                        {selectedConversation?.username}
+                    </span>
                 </div>
                 <Messages/>
                 <MessageInput/>
 
-            </>
+            </>)}
+
         </div>
     );
 };
+
+const NoChatSelected = () => {
+    return (
+        <div className="flex items-center justify-center w-full h-full">
+            <div
+                className="px-4 text-center sm:text-lg ms:text-xl text-slate-800 font-semibold flex flex-col items-center gap-2">
+                <p>반갑습니다.</p>
+
+                <p>채팅을 할려면, 유저를 클릭해주세요!</p>
+
+                <TiMessages className="text-3xl md:text-6xl text-center"/>
+            </div>
+        </div>
+    )
+}
 
 export default MessageContainer;
--- FE/src/components/sidebar/Conversations.jsx
+++ FE/src/components/sidebar/Conversations.jsx
@@ -1,16 +1,24 @@
 import Conversation from "./Conversation.jsx";
+import useGetConversations from "../../hooks/useGetConversations.js";
+import useConversation from "../../zustand/useConversation.js";
 
 const Conversations = () => {
+
+
+    const {loading, conversations} = useGetConversations()
     return (
         <div className='py-2 flex flex-col overflow-auto'>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
-            <Conversation/>
+            {conversations.map((conversation, idx) => {
+                return <Conversation
+                    conversation={conversation}
+                    key={conversation._id}
+                    lastIndex={idx === conversations.length - 1}
+                />;
+            })}
+            {loading ? (
+                <span className='loading loading-spinner'></span>
+            ) : null
+            }
         </div>
     );
 };
