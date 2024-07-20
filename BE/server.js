7aaf7f1f4920a6120dd0a27e1e572da2e9ca362a
김용민 <dydals3440@gmail.com>
2024-07-20 21:17:16 +0900

    feat: middleware to handle possible error

--- BE/server.js
+++ BE/server.js
@@ -34,3 +34,15 @@ app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     console.log("Server started on port" + PORT);
 });
+
+// error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
